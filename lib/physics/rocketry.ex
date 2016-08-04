17e7d52c0d32f8e3bd32f46829fb6c778ac467a6
Wojciech Bednarski <wbednarski@gmail.com>
2016-08-04 20:31:41 +0200

    converted code to more Elixirsts like

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,23 +1,42 @@
-defmodule Physics.Planet do
+defmodule Converter do
 
-  defstruct [
-    name: "Earth",
-    radius_m: 6.371e6,
-    mass_ks: 5.97e24
-  ]
-
-  def escape_velocity planet do
-    g = 6.67e-11
-    gmr = 2 * g * planet.mass_ks/planet.radius_m
-    vms = :math.sqrt gmr
-    vkms = vms/1000
-    Float.ceil vkms, 1
+  def rounded_to_nearest_tenth val do
+    Float.ceil val, 1
   end
+
+  def convert_to_km valocity do
+    valocity / 1000
+  end
+
+  def to_light_seconds {:miles, miles} do
+    (miles * 5.36819e-6) |> round_down
+  end
+
+  def to_light_seconds {:meters, meters} do
+    (meters * 3.335638620368e-9) |> round_down
+  end
+
+  def round_down(val) when is_float(val), do: trunc val
 
 end
 
-v = %Physics.Planet{} |> Physics.Planet.escape_velocity
-
 defmodule Physics.Rocketry do
 
+  def escape_velocity :earth do
+    %{mass: 5.972e24, radius: 6.371e6}
+    |> escape_velocity
+  end
+
+  def escape_velocity(planet) when is_map planet do
+    planet
+    |> calculate_escape
+    |> Converter.convert_to_km
+    |> Converter.rounded_to_nearest_tenth
+  end
+
+  defp calculate_escape %{mass: mass, radius: radius} do
+    newtons_constant = 6.67e-11
+    2 * newtons_constant * mass / radius
+    |> :math.sqrt
+  end
 end
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -2,7 +2,12 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "escape velocity of earth is correct" do
+    Physics.Rocketry.escape_velocity :earth
+  end
+
+  test "escape velocity of planet X is correct" do
+    %{mass: 4.0e22, radius: 6.21e6}
+    |> Physics.Rocketry.escape_velocity
   end
 end
